4584a1b50ad363ba42103d799a8fa31b5026523a
ryochi <212069@trident.ac.jp>
2022-12-23 20:36:26 +0900

    ブザーボタンを変更

--- public/kidds110-buzar/buzar.js
+++ public/kidds110-buzar/buzar.js
@@ -1,17 +1,18 @@
+let button_image = document.getElementById("button_image");
 function audio() {
-    if (!btn_audio.paused) {
-      btn_audio.pause();
-    }
-    else {
-      btn_audio.play();
-    } 
-
-    let btn = document.getElementsByClassName("btn")[0];
-    console.log(btn);
-    if (btn.imageContent === "") {
-      btn.textContent = "おふ！！";
-    } else {
-      btn.textContent = "おん！！";
-    }
-
+  if (!btn_audio.paused) {
+    btn_audio.pause();
   }
+  else {
+    btn_audio.play();
+  }
+  button_image.classList.toggle("ON");
+  // let btn = document.getElementsByClassName("btn")[0];
+  // console.log(btn);
+  // if (btn.imageContent === "おん！！") {
+  //   btn.textContent = "おふ！！";
+  // } else {
+  //   btn.textContent = "おん！！";
+  // }
+
+}
