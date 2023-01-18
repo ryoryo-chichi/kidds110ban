560120b6e708473a817716848eeb44ffb7bf3182
ryochi <212069@trident.ac.jp>
2023-01-18 10:57:03 +0900

    よくわかりません

---
+++ public/Root.js
@@ -0,0 +1,334 @@
+var latitude;
+var longitude;
+var array;
+var letter = [];
+var markers = [];
+var arraytest = "";
+var word = "";
+var finalanswer;
+var placeCount = [];
+var formAnswer;
+var infowindow;
+var queryString;
+var value1;
+var khajana;
+var map;
+var maker;
+var directionsDisplay;
+var directionsService;
+var response;
+var pinImage_red = new google.maps.MarkerImage(
+  "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+);
+var pinImage_blue = new google.maps.MarkerImage(
+  "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+);
+var MarkerArray = new google.maps.MVCArray();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlqNHdLwYQ_7o0UgXpMbQxt1d7l5F1H4s",
+  authDomain: "maps-42f90.firebaseapp.com",
+  databaseURL: "https://maps-42f90-default-rtdb.firebaseio.com",
+  projectId: "maps-42f90",
+  storageBucket: "maps-42f90.appspot.com",
+  messagingSenderId: "645482197750",
+  appId: "1:645482197750:web:980af93b0137d2c130b2d7",
+  measurementId: "G-67PQD2TSR7",
+};
+//initialize();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+//const docRef = firestore.doc("samples").ref();
+const saveButton = document.getElementById("saveButton");
+
+//座標を取得
+function test() {
+  navigator.geolocation.getCurrentPosition(test2);
+}
+
+function test2(position) {
+  //my code
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+
+  console.log(latitude);
+  console.log(longitude);
+}
+
+//test();
+
+//get all data where statement
+db.collection("users")
+  .where("answers", "==", "")
+  .get()
+  .then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      console.log(doc.data());
+    });
+  });
+
+//get all data
+// db.collection("users")
+//   .get()
+//   .then((snapshot) => {
+//     snapshot.docs.forEach((doc) => {
+//       console.log(doc.data());
+//     });
+//   });
+//現在地の座標をセーブするコード
+// saveButton.addEventListener("click", function () {
+//   console.log(latitude + "," + longitude);
+//   db.collection("users")
+//     .where("points", "==", latitude + "," + longitude)
+//     .get()
+//     .then((snapshot) => {
+//       snapshot.docs.forEach((doc) => {
+//         console.log(doc.data());
+//         khajana = doc.data();
+//         console.log({ khajana });
+//       });
+//       if (khajana) {
+//         alert(`${latitude + "," + longitude}この座標を既に登録されています`);
+//       } else {
+//         db.collection("users").add({
+//           answers: "",
+//           points: latitude + "," + longitude,
+//         });
+//         alert(`${latitude + "," + longitude}この座標を登録しました`);
+//         refresh();
+//       }
+//     });
+// });
+
+//refersh page
+function refresh() {
+  setTimeout(function () {
+    location.reload();
+  }, 2000);
+}
+
+//get all data from firebase
+
+db.collection("users")
+  .get()
+  .then((snapshot) => {
+    snapshot.docs.map((doc) => {
+      console.log(doc.data().points);
+      arraytest = doc.data().points;
+      console.log(arraytest);
+      placeCount.push(arraytest);
+      word += arraytest + ":";
+    });
+    test();
+    initMap();
+  });
+//マップ生成？
+function initMap() {
+  // Map option
+
+  var options = {
+    center: { lat: 35.1725302, lng: 136.8865799 },
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  };
+
+  //New Map
+  map = new google.maps.Map(document.getElementById("map"), options);
+
+  directionsDisplay = new google.maps.DirectionsRenderer({
+    polylineOptions: {
+      //strokeColor: '#FF0000',
+      strokeColor: "#1e90ff",
+      strokeWeight: 6,
+      strokeOpacity: 0.6,
+    },
+  });
+  //listen for click on map location
+
+  console.log("placeCount " + placeCount.length);
+
+  //for
+  for (i = 0; i < placeCount.length; i++) {
+    var answer = word.split(/:/)[i];
+    console.log(word.split(/:/)[i]);
+    console.log(answer);
+
+    finalanswer = answer;
+    var finalanswer = finalanswer.split(/,|\s/);
+
+    console.log(finalanswer[0]);
+    console.log(finalanswer[1]);
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(finalanswer[0], finalanswer[1]),
+      map: map,
+
+      //draggable:true,
+      //icon:'/scripts/img/logo-footer.png'
+    });
+    marker.addListener("click", function (e) {
+      // data form firebase
+      var touchPoint = e.latLng.lat() + "," + e.latLng.lng();
+
+      //urlで座標を別のページに送る
+      value1 = e.latLng.lat() + "," + e.latLng.lng();
+      queryString = "?para1=" + value1;
+
+      console.log("querystring " + queryString);
+
+      db.collection("users")
+        .where("points", "==", touchPoint)
+        .get()
+        .then((snapshot) => {
+          snapshot.docs.forEach((doc) => {
+            console.log(doc.data());
+            formAnswer = doc.data().answers;
+
+            console.log("formAnswer " + formAnswer);
+            console.log("touchPoint " + touchPoint);
+          });
+          if (formAnswer && formAnswer.length > 0) {
+            console.log("formAnswer " + formAnswer);
+            //座標に入っているアンケート結果を表示す
+
+            var contentString_Show = `<h2>アンケート結果</h2> <h3>家にいる日</h3>
+            <li>${formAnswer}</li>`;
+
+            var infoWindow_Show = new google.maps.InfoWindow({
+              content: contentString_Show,
+            });
+
+            infoWindow_Show.open(map, this);
+          } else {
+            // "&para2=" + value2;
+            // window.location.href =
+            //   "https://formtest12-19.web.app/form.html" + queryString;
+
+            //アンケートが未回答なのでinfoWindow_Formを表示してアンケート画面に誘導
+
+            infoWindow_Form.open(map, this);
+          }
+
+          console.log(e.latLng.lat());
+          console.log(e.latLng.lng());
+        });
+
+      var contentString_Form = ` '<h1 id="firstHeading" class="firstHeading">アンケート画面</h1>' 
+    <a href="https://formtest12-19.web.app/form.html?${queryString}" >form画面</a>`;
+
+      var infoWindow_Form = new google.maps.InfoWindow({
+        content: contentString_Form,
+      });
+    });
+  }
+  directionsDisplay = new google.maps.DirectionsRenderer({
+    polylineOptions: {
+      //strokeColor: '#FF0000',
+      strokeColor: "#1e90ff",
+      strokeWeight: 6,
+      strokeOpacity: 0.6,
+    },
+  });
+  directionsDisplay.setMap(map);
+
+  // createMarker関数の呼び出し
+  createMarker();
+}
+//ルート検索
+function searchRoute() {
+  var start = document.getElementById("start").value;
+  var end = document.getElementById("end").value;
+
+  var rendererOptions = {
+    draggable: true,
+    preserveViewport: false,
+  };
+
+  directionsService = new google.maps.DirectionsService();
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: google.maps.DirectionsTravelMode.DRIVING, // 自動車でのルート
+    unitSystem: google.maps.DirectionsUnitSystem.METRIC, // 単位km表示
+    optimizeWaypoints: true, // 最適化された最短距離にする
+    avoidHighways: true, // 高速道路を除外
+    avoidTolls: true, // 有料道路を除外
+  };
+  directionsService.route(request, function (response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+      directionsDisplay.setPanel(document.getElementById("directionsPanel"));
+    }
+    //overview_pathを表示する
+    // for (var i = 0; i < response.routes.length; i++) {
+    //   var r = response.routes[i];
+    //   for (var j = 0; j < r.overview_path.length; j++) {
+    //     var latlng = r.overview_path[j];
+    //     marker = new google.maps.Marker({
+    //       position: latlng,
+    //       icon: pinImage_red,
+    //       map: map
+    //     });
+    //     markers.push(marker);
+    //   }
+    // }
+  });
+  map = new google.maps.Map(document.getElementById("map_canvas"), response);
+  directionsDisplay.setMap(map);
+  //createMarker();
+}
+//createMarker関数
+function createMarker() {
+  // mapをクリックしたときのイベントを設定
+  google.maps.event.addListener(map, "click", maplistener);
+
+  // mapをクリックしたときの処理
+  function maplistener(event) {
+    // marker作成
+    marker = new google.maps.Marker({
+      map: map,
+      draggable: true, // ドラッグを可能にする
+      icon: pinImage_blue,
+    });
+    // markerの位置を設定
+    // event.latLng.lat()でクリックしたところの緯度を取得
+    marker.setPosition(
+      new google.maps.LatLng(event.latLng.lat(), event.latLng.lng())
+    );
+    MarkerArray.push(marker);
+  }
+}
+//deleteMarkers関数
+function deleteMarkers() {
+  MarkerArray.forEach(function (marker, idx) {
+    marker.setMap(null);
+  });
+  MarkerArray = []; // marker配列の初期化
+}
+//csvOutput関数
+function csvOutput() {
+  MarkerArray.forEach(function (marker, idx) {
+    var pos = marker.getPosition();
+    var lat = pos.lat();
+    var lng = pos.lng();
+    $("#csv").append(lat + "," + lng + "<br />");
+  });
+}
+//csvDelete関数
+function csvDelete() {
+  $("#csv").empty();
+}
+//invisibleMarkers関数
+function invisibleMarkers() {
+  markers.forEach(function (marker, idx) {
+    marker.setVisible(false);
+  });
+}
+//visibleMarkers関数
+function visibleMarkers() {
+  markers.forEach(function (marker, idx) {
+    marker.setVisible(true);
+  });
+}
---
+++ public/form.js
@@ -0,0 +1,85 @@
+//firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBlqNHdLwYQ_7o0UgXpMbQxt1d7l5F1H4s",
+  authDomain: "maps-42f90.firebaseapp.com",
+  databaseURL: "https://maps-42f90-default-rtdb.firebaseio.com",
+  projectId: "maps-42f90",
+  storageBucket: "maps-42f90.appspot.com",
+  messagingSenderId: "645482197750",
+  appId: "1:645482197750:web:980af93b0137d2c130b2d7",
+  measurementId: "G-67PQD2TSR7",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+//get points data from maps
+//マーカを押した時にアンケートの結果が空だったらその場合の座標をindex.jsからurlで受けとる
+var queryString = decodeURIComponent(window.location.search);
+queryString = queryString.substring(1);
+var queries = queryString.split("&");
+var queries = queryString.split("para1=");
+
+//imp code
+//index.jsからurlで受け取った座標をspkitで分解する
+console.log("queryString " + queryString);
+
+var next = queries[1].split(",");
+var last = next[0] + "," + next[1];
+
+console.log(last);
+
+// checkboxの情報
+document.querySelectorAll('input[type="checkbox"]:checked');
+const body = document.querySelector("body");
+var selected = [];
+
+body.addEventListener("change", (event) => {
+  if (event.target.type === "checkbox") {
+    const checked = document.querySelectorAll('input[type="checkbox"]:checked');
+    selected = Array.from(checked).map((x) => x.value);
+  }
+  console.log(selected);
+});
+
+// アンケートが未回答IDを取得してアンケートの結果を保存する
+var id;
+
+document.getElementById("buttonSend").addEventListener("click", function () {
+  //index.jsで送った座標を使っていデータベースアンケートの中未回答のIDを探す
+  db.collection("users")
+    .where("points", "==", last)
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        console.log(doc.data());
+        console.log(doc.id);
+        id = doc.id;
+
+        // whereで検索して座標のidにアンケートの結果を保存
+        console.log("selected.length" + selected.length);
+        if (selected.length > 0) {
+          db.collection("users").doc(id).update({
+            answers: selected,
+          });
+          alert(`アンケート結果が保存しました。`);
+          refreshPage();
+        } else {
+          alert(`アンケート結果を入力してから保存してください。`);
+        }
+      });
+    });
+});
+//アンケート結果を後で答える時に追加したアンケート結果をすぐに地図に反映するコード
+//ページをreloadしている
+function refreshPage() {
+  setTimeout(function () {
+    //  href = "https://formtest12-19.web.app/".location.reload();
+    window.location.href = "https://formtest12-19.web.app/main.js";
+  }, 3000);
+}
+
+// document.getElementById("buttonReturn").addEventListener("click", function () {
+//   window.location.href = "https://formtest12-19.web.app/main.js";
+// });
